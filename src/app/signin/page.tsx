ae05ca22c787740db671570b0fb82f1989e4199c
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-10-14 10:16:31 +0200

    Update signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,50 +1,32 @@
 "use client"
 import {Card, Form, Container, Button} from "react-bootstrap";
-import {useState} from "react";
-import { redirect } from 'next/navigation'
+import {useState, FormEvent} from "react";
+import { useRouter } from 'next/navigation';
+import './style.scss'
 
 export default function SignIn() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const router = useRouter();
 
-    const connectionHandler = async (e) => {
+    const connectionHandler = async (e: FormEvent) => {
         e.preventDefault();
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signin`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username, password}),
+            credentials: 'include'
+        });
 
-        try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signin`, {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({username, password}),
-                credentials: 'include'
-            });
-
-            const data = await response.json();
-
-            console.log(data)
-
-            if (!response.ok) {
-                alert(data.message || 'Erreur de connexion');
-                return;
-            }
-
-            // Exemple : stocker le token dans le localStorage
-            if (data.token) {
-                alert('Je rentre la')
-                localStorage.setItem('token', data.token);
-                redirect('/')
-            }else {
-                alert('Je ne rentre pas la')
-            }
-
-        } catch (error) {
-            console.error('Erreur lors de la connexion :', error);
-            alert('Impossible de se connecter au serveur.');
+        if (!response.ok) {
+            return;
         }
+        router.push('/');
     };
 
     return (
         <Container className="d-flex justify-content-center align-items-center vh-100">
-            <Card style={{width: '30rem'}}>
+            <Card className="signin-card">
                 <Card.Body>
                     <Card.Title className="mb-4 text-center">Connexion</Card.Title>
                     <Form onSubmit={connectionHandler}>
