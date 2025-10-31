5bcf317021e88b88534c754d67307b31bf3e450e
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-10-31 10:05:31 +0100

    Add BBCode translator

---
+++ src/lib/bbcode.ts
@@ -0,0 +1,26 @@
+import yabbcode from 'ya-bbcode'
+
+const parser = new yabbcode();
+parser.registerTag('align', {
+    type: "replace",
+    open: function(direction) {
+        return `<div style="text-align: ${direction}">`
+    },
+    close: function() {
+        return "</div>"
+    }
+})
+parser.registerTag('color', {
+    type: "replace",
+    open: function(color) {
+        return `<div style="color: ${color}">`
+    },
+    close: function() {
+        return "</div>"
+    }
+})
+
+export function convertToHtml(bbcode: string) {
+    const html = parser.parse(bbcode);
+    return html
+}
