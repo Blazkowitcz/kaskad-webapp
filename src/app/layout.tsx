e68e57e33a2ffac726b78192838d84069c83a56c
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-11-03 08:39:12 +0100

    Redirect to main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,32 +2,32 @@
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Navbar, Container, Nav, NavDropdown, Form, InputGroup} from "react-bootstrap"
-import { AuthContextProvider } from "@/app/context/AuthContext";
+import {AuthContextProvider} from "@/app/context/AuthContext";
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-      <Navbar expand="lg" className="bg-body-tertiary test">
-          <Navbar.Brand href="#home">Kaskad</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                  <Nav.Link href="/torrents">Torrents</Nav.Link>
-                  <Form>
-                      <InputGroup>
-                          <Form.Control placeholder={`Search`} aria-label="Search"/>
-                      </InputGroup>
-                  </Form>
-              </Nav>
-          </Navbar.Collapse>
-      </Navbar>
+    return (
+        <html lang="en">
+        <body>
+        <Navbar expand="lg" className="bg-body-tertiary test">
+            <Navbar.Brand href="/">Kaskad</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/torrents">Torrents</Nav.Link>
+                    <Form>
+                        <InputGroup>
+                            <Form.Control placeholder={`Search`} aria-label="Search"/>
+                        </InputGroup>
+                    </Form>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
         <AuthContextProvider>{children}</AuthContextProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
