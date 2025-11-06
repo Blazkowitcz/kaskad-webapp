c2d6705f67cdbc08b57a087fc2754f4df7065ed9
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-11-06 07:37:03 +0100

    Add fields constants

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+export enum FIELDS {
+    NAME = 'name',
+    SLUG = 'slug',
+    DESCRIPTION = 'description',
+    CATEGORIES = 'categories',
+    CATEGORY = 'category',
+    SUBCATEGORIES = 'subcategories',
+    SUBCATEGORY = 'subcategory',
+    AUTHOR = 'author',
+    USER = 'user',
+    ID = 'id',
+    CREATED_AT = 'createdAt',
+    SIZE = 'size',
+    COMPLETED = 'completed',
+    LANGUAGES = 'languages',
+}
