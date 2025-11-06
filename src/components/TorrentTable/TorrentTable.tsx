09d4c5324bdd7758715d37b422bc045f69539c17
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-11-06 07:37:23 +0100

    Update TorrentTable component

--- src/components/TorrentTable/TorrentTable.tsx
+++ src/components/TorrentTable/TorrentTable.tsx
@@ -6,7 +6,7 @@ import './style.scss'
 
 export default function TorrentTable({torrents}: { torrents: Torrent[] }) {
     return (
-        <Table striped bordered>
+        <Table striped bordered={false} hover responsive className="table">
             <thead>
             <tr>
                 <th>Type</th>
@@ -18,19 +18,26 @@ export default function TorrentTable({torrents}: { torrents: Torrent[] }) {
             </tr>
             </thead>
             <tbody>
-            {torrents.map((torrent: Torrent) => {
-                return (
-                    <tr key={torrent.id}>
-                        <td className="table-cell">{<Badge>{torrent.subcategory.name}</Badge>}</td>
-                        <td className="table-cell">{<a href={`/torrents/${torrent.slug}`}>{torrent.name}</a>}</td>
-                        <td className="table-cell">{formatDistance(new Date(torrent.createdAt), new Date(), {addSuffix: true})}</td>
-                        <td className="table-cell">{filesize(torrent.size)}</td>
-                        <td className="table-cell">{torrent.completed}</td>
-                        <td className="table-cell">{<Badge>{torrent.seeders}</Badge>} / {<Badge>0</Badge>}</td>
-                    </tr>)
-            })}
+            {torrents.map((torrent: Torrent) => (
+                <tr key={torrent.id}>
+                    <td className="table-cell">
+                        <span className="badge">{torrent.subcategory.name}</span>
+                    </td>
+                    <td className="table-cell">
+                        <a href={`/torrents/${torrent.slug}`}>{torrent.name}</a>
+                    </td>
+                    <td className="table-cell">
+                        {formatDistance(new Date(torrent.createdAt), new Date(), {addSuffix: true})}
+                    </td>
+                    <td className="table-cell">{filesize(torrent.size)}</td>
+                    <td className="table-cell">{torrent.completed}</td>
+                    <td className="table-cell">
+                        <span className="badge seeders">{torrent.seeders}</span>{' '}
+                        / <span className="badge leechers">0</span>
+                    </td>
+                </tr>
+            ))}
             </tbody>
         </Table>
     )
-
 }
