ac4151b968b1d2d7bd01a4e97a55639213aaf788
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-11-06 07:38:58 +0100

    Update layout and main CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
+
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Navbar, Container, Nav, NavDropdown, Form, InputGroup} from "react-bootstrap"
+import {Navbar, Container, Nav, Form, InputGroup, Button} from "react-bootstrap";
 import {AuthContextProvider} from "@/app/context/AuthContext";
 
 export default function RootLayout({
@@ -12,20 +13,29 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body>
-        <Navbar expand="lg" className="bg-body-tertiary test">
-            <Navbar.Brand href="/">Kaskad</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/torrents">Torrents</Nav.Link>
-                    <Form>
+        <Navbar expand="lg" className="navbar">
+            <Container fluid>
+                <Navbar.Brand href="/">Kaskad</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/torrents">Torrents</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link className="btn btn-primary" href="/torrents/create">Upload Torrent</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
                         <InputGroup>
-                            <Form.Control placeholder={`Search`} aria-label="Search"/>
+                            <Form.Control
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
                         </InputGroup>
                     </Form>
-                </Nav>
-            </Navbar.Collapse>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
+
         <AuthContextProvider>{children}</AuthContextProvider>
         </body>
         </html>
